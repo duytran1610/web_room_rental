392dc268e410c71d5bf42478f971b99f5a99416f
duytran1610 <dukhoa4.clknn@gmail.com>
2023-08-18 18:34:30 +0700

    move the original files to the client directory

--- server/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import authController from '../controllers/authController';
-
-const authRouter = express.Router();
-
-authRouter.post('/register', authController.register);
-authRouter.post('/login', authController.login);
-
-export default authRouter;
