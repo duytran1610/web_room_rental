3732d5fc759e073c0e342c60d3e69a6d3f4c4e1d
duytran1610 <dukhoa4.clknn@gmail.com>
2024-02-06 23:37:29 +0300

    Fix component YMap

--- client/src/components/YMap.js
+++ client/src/components/YMap.js
@@ -1,37 +1,72 @@
-import React, { memo, useState} from 'react';
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import React, { memo, useState, useEffect } from 'react';
+import { YMaps, Map, Placemark, Button } from '@pbe/react-yandex-maps';
 
 // default value for ymap
 const mapState = {
-    center: [55.75, 37.57],
+    center: [21.0245, 105.84117],
     zoom: 11,
-    controls: ['zoomControl', 'fullscreenControl']
 };
 
-// create ymap
 const YMap = ({ address }) => {
-    // coordinates of object in  ymap
-    const [addressCoord, setAddressCoord] = useState();
+    const ymaps = React.useRef(null);
+    const placemarkRef = React.useRef(null);
+    const mapRef = React.useRef(null);
 
-    let ymapsObject;
+    // create Placemark
+    const createPlacemark = (coords) => {
+        return new ymaps.current.Placemark(
+            coords,
+            {
+                iconCaption: "loading.."
+            },
+            {
+                preset: "islands#violetDotIconWithCaption",
+                draggable: true
+            }
+        );
+    };
 
-    const onYmapsLoad = (ymaps) => {
-        ymapsObject = ymaps;
-        getCoords();
-    }
+    // get address by coords
+    const getAddress = (coords) => {
+        let regionName = "";
+        placemarkRef.current.properties.set("iconCaption", "loading..");
 
-    const getCoords = async () => {
-        try {
-            let myGeocoder = await ymapsObject.geocode('338 Nguyen Van Qua Street, Dong Hung Thuan Ward, District 12, Ho Chi Minh');
-            let coord = myGeocoder.geoObjects.get(0).geometry.getCoordinates();
-            setAddressCoord(coord);
-        } catch (err) {
-            console.log(err);
+        ymaps.current.geocode(coords).then((res) => {
+            const firstGeoObject = res.geoObjects.get(0);
+            const newAddress = [
+                firstGeoObject.getLocalities().length
+                    ? firstGeoObject.getLocalities()
+                    : firstGeoObject.getAdministrativeAreas(),
+                firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
+            ]
+                .filter(Boolean)
+                .join(", ");
+
+            regionName = firstGeoObject.getAdministrativeAreas()[0];
+
+            placemarkRef.current.properties.set({
+                iconCaption: newAddress,
+                balloonContent: regionName
+            });
+        })
+    };
+
+    // envent click map
+    const onMapClick = (e) => {
+        const coords = e.get("coords");
+        if (placemarkRef.current) {
+            placemarkRef.current.geometry.setCoordinates(coords);
+        } else {
+            placemarkRef.current = createPlacemark(coords);
+            mapRef.current.geoObjects.add(placemarkRef.current);
+            placemarkRef.current.events.add("dragend", function () {
+                getAddress(placemarkRef.current.geometry.getCoordinates());
+            });
         }
-    }
+        getAddress(coords);
+    };
 
     return (
-        // Important! Always set the container height explicitly
         <div className='h-[300px] w-full relative'>
             {address &&
                 <div className='absolute top-2 left-2 z-50 max-w-[200px] bg-white shadow-md p-2 text-xs rounded-md'>
@@ -39,25 +74,25 @@ const YMap = ({ address }) => {
                 </div>
             }
 
-            <YMaps query={{
-                ns: 'use-load-option',
-                load: 'Map,Placemark,control.ZoomControl,control.FullscreenControl,geoObject.addon.balloon',
-                lang: 'en_RU',
-                apikey: process.env.REACT_APP_YMAP_API
-            }}>
+            <YMaps
+                query={{
+                    lang: 'en_RU',
+                    apikey: "106e4ebf-abbc-41b9-9230-adfbc64f15d9"
+                }}>
                 <Map
-                    onLoad={ymaps => onYmapsLoad(ymaps)}
-                    modules={["geocode"]}
+                    modules={["Polygon", "GeoObject", "geoQuery", "control.ZoomControl", "control.FullscreenControl", "Placemark", "geocode",
+                        "geoObject.addon.balloon", "borders", "ObjectManager", 'geoObject.addon.balloon', 'clusterer.addon.balloon',
+                        'templateLayoutFactory']}
                     width="100%"
                     height="100%"
-                    state={addressCoord ? { ...mapState, center: addressCoord } : mapState}
+                    onClick={onMapClick}
+                    instanceRef={mapRef}
+                    onLoad={(ymapsInstance) => (ymaps.current = ymapsInstance)}
+                    state={mapState}
                 >
-                    <Placemark
-                        geometry={addressCoord}
-                    />
                 </Map>
             </YMaps>
-        </div>
+        </div >
     )
 }
 
